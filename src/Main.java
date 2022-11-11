f73f66effca90dc4ba0a4d38af369347a5658b31
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-11-11 19:33:12 +0500

    Mastermind

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,7 @@ public class Main {
                 >0123
         Congrats! You did it!*/
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         //String secretCode = sc.nextLine();
         //random code generator 4 digits long with no repeating digits
         StringBuilder secretCode = new StringBuilder();
@@ -33,7 +33,9 @@ public class Main {
             }
             secretCode.append(digit);
         }
-        System.out.println(secretCode);
+        System.out.println("this code should not actually be visible to you! |-> "+secretCode+" <-|");
+        System.out.println("-------------------------------------------------------------");
+
         int wellPlacedPieces = 0;
         int misplacedPieces = 0;
         int round = 0;
@@ -41,17 +43,28 @@ public class Main {
         boolean isCorrect = false;
         System.out.println("Will you find the secret code? 🤓🤓🤓");
         while (!isCorrect) {
-            System.out.println("---");
+            System.out.println("____");
             System.out.println("Round " + round);
             if (round == 11) {
                 System.out.println("You lost! 😭😭😭");
                 break;
             }
-            guess = sc.nextLine();
+            guess = scanner.nextLine();
+            if (guess.length() != 4) {
+                System.out.println("code must be 4 digits!");
+                continue;
+            }
+
+            if (!guess.matches("[0-9]+")) {
+                System.out.println("must consist of numbers only");
+                continue;
+            }
+
             if (guess.length() != secretCode.length()) {
                 System.out.println("Wrong input!");
                 continue;
             }
+
             for (int i = 0; i < guess.length(); i++) {
                 if (guess.charAt(i) == secretCode.charAt(i)) {
                     wellPlacedPieces++;
